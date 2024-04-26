b321bbf50d2675182c942e0b0312eeee430cc9a5
Aamir Farooq <SlimShadyIAm@users.noreply.github.com>
2024-04-26 14:06:24 +0200

    Keycloak theme: fix broken button text on info page (#43)

--- keycloak-theme/src/login/i18n.ts
+++ keycloak-theme/src/login/i18n.ts
@@ -8,6 +8,8 @@ export const { useI18n } = createUseI18n({
     // that is "Forgot Password?" see: https://github.com/InseeFrLab/keycloakify/blob/f0ae5ea908e0aa42391af323b6d5e2fd371af851/src/lib/i18n/generated_messages/18.0.1/login/en.ts#L17
     doForgotPassword: 'I forgot my password',
     invalidUserMessage: 'Invalid username or password.',
+    backToApplication: "Back to application",
+    proceedWithAction: "Click here to proceed"
   },
 });
 
