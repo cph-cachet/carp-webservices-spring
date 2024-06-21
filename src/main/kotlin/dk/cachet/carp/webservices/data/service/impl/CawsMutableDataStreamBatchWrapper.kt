3eb45f29b0dc5c1105397ec05d38c5a2567c861d
davidscavnicky <dave.scavnicky@gmail.com>
2024-06-21 11:49:44 +0200

    no premature optimization and sequential checks

--- src/main/kotlin/dk/cachet/carp/webservices/data/service/impl/CawsMutableDataStreamBatchWrapper.kt
+++ src/main/kotlin/dk/cachet/carp/webservices/data/service/impl/CawsMutableDataStreamBatchWrapper.kt
@@ -2,6 +2,8 @@ package dk.cachet.carp.webservices.data.service.impl
 
 import dk.cachet.carp.common.application.data.Data
 import dk.cachet.carp.data.application.*
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 
 /** UNDER CONSTRUCTION --> CHECK create a function that checks sequence IDs before appending
  * + usable if we can get back last preceding upload of a sequence so the client know what to send
@@ -9,6 +11,10 @@ import dk.cachet.carp.data.application.*
  * all the sequence IDs in the append method. This is a temporary solution until we find a better way to handle this.
  */
 class CawsMutableDataStreamBatchWrapper : Sequence<DataStreamPoint<*>>, DataStreamBatch {
+    companion object {
+        private val LOGGER: Logger = LogManager.getLogger()
+    }
+
     private val sequenceMap: MutableMap<DataStreamId, MutableList<MutableDataStreamSequence<*>>> = mutableMapOf()
 
     /**
@@ -41,44 +47,10 @@ class CawsMutableDataStreamBatchWrapper : Sequence<DataStreamPoint<*>>, DataStre
 
     /**
      * Append all data stream sequences contained in [batch] to this batch.
-     *
-     * no catch to throw "IllegalArgumentException" when the start of any sequences contained in [batch]
-     * precede the end of a previously appended sequence to the same data stream. --> This is a temporary solution
+     * rid of premature optimization and sequential checks. <-- let fly in anything and check later. --> This is a temporary solution
      */
 
     fun appendBatch(batch: DataStreamBatch) {
-        val containsNoPrecedingSequence =
-            when (batch) {
-                is CawsMutableDataStreamBatchWrapper ->
-                    // Preconditions for `MutableDataStreamBatch` can be verified much more easily.
-                    // This might seem like premature optimization, but currently it is the only concrete class.
-                    // We expect many sequences for one data type to be common,
-                    // e.g., RR intervals have many sync points.
-                    batch.sequenceMap
-                        .mapValues { it.value.last() }
-                        .all { (dataStream, lastSequence) ->
-                            val lastStoredSequence = sequenceMap[ dataStream ]?.last()
-                            if (lastStoredSequence == null) {
-                                true
-                            } else {
-                                lastStoredSequence.range.last < lastSequence.range.first
-                            }
-                        }
-                else ->
-                    batch.sequences.all { sequence ->
-                        sequenceMap[ sequence.dataStream ]?.last().let { lastStoredSequence ->
-                            if (lastStoredSequence == null) {
-                                true
-                            } else {
-                                lastStoredSequence.range.last < sequence.range.first
-                            }
-                        }
-                    }
-            }
-        require(containsNoPrecedingSequence) {
-            "The batch contains a sequence of which the start precedes a previously appended sequence"
-        }
-
         batch.sequences.forEach(::appendSequence)
     }
 
