2cc415229f85abad9d14f88ffb3867c43349a90a
davidscavnicky <dave.scavnicky@gmail.com>
2024-06-24 11:03:32 +0200

    info

--- src/main/kotlin/dk/cachet/carp/webservices/data/service/impl/DataStreamServiceWrapper.kt
+++ src/main/kotlin/dk/cachet/carp/webservices/data/service/impl/DataStreamServiceWrapper.kt
@@ -51,7 +51,10 @@ class DataStreamServiceWrapper(
 
         return sortedDataPoint.updatedAt?.toKotlinInstant()
     }
-
+                // IO dispatcher needed for file operations to optimize
+                // input/output operations and offloading blocking operations
+                // Function takes zipped file unzip it and extract
+                // the data from it into DataStreamServiceRequest ( CORE )
     suspend fun extractFilesFromZip(zipFile: ByteArray): DataStreamServiceRequest<*>? =
         withContext(Dispatchers.IO) {
             val objectMapper = ObjectMapper()
@@ -71,7 +74,8 @@ class DataStreamServiceWrapper(
                         val content = source.buffer().readByteArray()
                         val snapshot: JsonNode = objectMapper.readTree(content)
 
-                        dataStreamServiceRequest = JSON.decodeFromString(DataStreamServiceRequest.Serializer, snapshot.toString())
+                        dataStreamServiceRequest = JSON.decodeFromString(
+                            DataStreamServiceRequest.Serializer, snapshot.toString())
                     }
                 }
 
@@ -85,6 +89,8 @@ class DataStreamServiceWrapper(
             return@withContext dataStreamServiceRequest
         }
 
+        // function calls the core service method with the extracted data from the zip file
+        // and subsequently invokes the core service method
     override suspend fun processZipToInvoke(zipFile: MultipartFile): Any {
         val zipFileBytes = zipFile.bytes
 
