610aab9d1d4b6c1f2971415f179143adff382ab4
davidscavnicky <dave.scavnicky@gmail.com>
2024-06-25 15:17:35 +0200

    Caws..Wrapper to @Service w Spring; so open for ini in tes

--- src/test/kotlin/dk/cachet/carp/webservices/data/service/impl/DataStreamServiceWrapperTest.kt
+++ src/test/kotlin/dk/cachet/carp/webservices/data/service/impl/DataStreamServiceWrapperTest.kt
@@ -1,23 +1,26 @@
 package dk.cachet.carp.webservices.data.service.impl
 
+import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.ObjectMapper
 import dk.cachet.carp.common.application.UUID
-import dk.cachet.carp.common.application.services.ApiVersion
 import dk.cachet.carp.data.infrastructure.DataStreamServiceDecorator
 import dk.cachet.carp.data.infrastructure.DataStreamServiceRequest
 import dk.cachet.carp.webservices.common.configuration.internationalisation.service.MessageBase
 import dk.cachet.carp.webservices.common.services.CoreServiceContainer
 import dk.cachet.carp.webservices.data.serdes.DataStreamServiceRequestSerializer
 import dk.cachet.carp.webservices.data.service.core.CoreDataStreamService
+import io.mockk.Runs
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withContext
 import okio.IOException
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.*
 import org.springframework.web.multipart.MultipartFile
 import java.io.ByteArrayOutputStream
+import java.util.concurrent.TimeUnit
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 import kotlin.test.assertFailsWith
@@ -26,17 +29,23 @@ import kotlin.test.assertNull
 class DataStreamServiceWrapperTest {
     @Nested
     inner class ExtractFilesFromZip {
+        @BeforeEach
+        fun setup() {
+            System.setProperty("kotlinx.coroutines.debug", "off")
+        }
+
         @Test
+        @Timeout(value = 20, unit = TimeUnit.SECONDS)
         fun `should extract files from valid zip`() =
             runTest {
-                // Arrange
-                // Mock the DataStreamServiceRequest
-                val dataStreamServiceRequest = DataStreamServiceRequest.AppendToDataStreams(
-                    studyDeploymentId = UUID.randomUUID(),
-                    batch = CawsMutableDataStreamBatchWrapper()
-                )
+                // Need for actual initiaziation inside a test so the .zip
+                // file can be created
 
-                every { dataStreamServiceRequest.apiVersion } returns ApiVersion(1, 2)
+                // Arrange
+                val studyDeploymentId = UUID.randomUUID()
+                val dataStreamBatch = CawsMutableDataStreamBatchWrapper() // replace with actual data
+                val dataStreamServiceRequest =
+                    DataStreamServiceRequest.AppendToDataStreams(studyDeploymentId, dataStreamBatch)
 
                 // Convert the mock to a byte array
                 val objectMapper = ObjectMapper()
@@ -45,7 +54,20 @@ class DataStreamServiceWrapperTest {
                 every { messageBase.get(any(), any()) } returns "Serializer fails somewhere"
 
                 val serializer = DataStreamServiceRequestSerializer(messageBase)
-                val requestAsJson = objectMapper.writeValueAsString(serializer.serialize(dataStreamServiceRequest, mockk(), mockk()))
+
+                val jsonGenerator = mockk<JsonGenerator>()
+                every { jsonGenerator.writeRawValue(any<String>()) } just Runs
+                val requestAsJson =
+                    withContext(Dispatchers.IO) {
+                        objectMapper.writeValueAsString(
+                            serializer.serialize(
+                                dataStreamServiceRequest,
+                                jsonGenerator,
+                                mockk(),
+                            ),
+                        ).toString()
+                    }
+
                 val requestBytes = requestAsJson.toByteArray()
 
                 // Zip the byte array
@@ -71,10 +93,11 @@ class DataStreamServiceWrapperTest {
                         mockk(),
                         services,
                     )
-
                 // Act & Assert
                 assertDoesNotThrow {
-                    sut.extractFilesFromZip(zippedRequestBytes)
+                    withContext(Dispatchers.IO) {
+                        sut.extractFilesFromZip(zippedRequestBytes)
+                    }
                 }
             }
 
@@ -91,7 +114,11 @@ class DataStreamServiceWrapperTest {
 
                 val services = mockk<CoreServiceContainer>()
                 val coreDataStreamService = mockk<CoreDataStreamService>()
-                every { services.dataStreamService } returns DataStreamServiceDecorator(coreDataStreamService, mockk())
+                every { services.dataStreamService } returns
+                    DataStreamServiceDecorator(
+                        coreDataStreamService,
+                        mockk(),
+                    )
 
                 // Mock the behavior of zipFile here
                 val sut =
@@ -127,5 +154,5 @@ class DataStreamServiceWrapperTest {
                 // Assert
                 assertNull(result)
             }
-       }
+    }
 }
