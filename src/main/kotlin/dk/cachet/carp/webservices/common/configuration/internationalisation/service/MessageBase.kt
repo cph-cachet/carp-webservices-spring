682d0bffdeee3ec1647391ff81b46e06b3d0f4a2
Yuan Chen <10835085+yuanchen233@users.noreply.github.com>
2024-08-14 15:19:18 +0200

    Clean up document and collection service (#117)
    * Filter teams notifications from Dev server
    
    * Refactor DocumentTraverser
    
    * Apply server-side analysis
    
    * Fix MessageBase Bean
    
    * Document Inactive deployments endpoint

--- src/main/kotlin/dk/cachet/carp/webservices/common/configuration/internationalisation/service/MessageBase.kt
+++ src/main/kotlin/dk/cachet/carp/webservices/common/configuration/internationalisation/service/MessageBase.kt
@@ -3,6 +3,7 @@ package dk.cachet.carp.webservices.common.configuration.internationalisation.ser
 import dk.cachet.carp.webservices.common.configuration.internationalisation.AMessageBase
 import org.springframework.context.MessageSource
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.support.MessageSourceAccessor
 import org.springframework.context.support.ResourceBundleMessageSource
 import org.springframework.stereotype.Service
@@ -13,19 +14,21 @@ import org.springframework.stereotype.Service
  * and internationalization of the messages.
  */
 @Service
-class MessageBase : AMessageBase() {
-    init
-    {
-        // Initialize the [messageSource]
-        accessor = MessageSourceAccessor(messageSource())
+class MessageBase(messageSource: MessageSource) : AMessageBase() {
+    init {
+        // Initialize the [accessor] with the injected [messageSource]
+        accessor = MessageSourceAccessor(messageSource)
     }
+}
 
+@Configuration
+class MessageConfig {
     /**
      * The function [messageSource] implements the [MessageSource] interface for resolving messages,
      * @return The [ResourceBundleMessageSource] with the given Map that is keyed with the message code.
      */
     @Bean
-    final fun messageSource(): MessageSource {
+    fun messageSource(): MessageSource {
         val resourceBundleMessageSource = ResourceBundleMessageSource()
         resourceBundleMessageSource.setBasename("messages/messages")
         resourceBundleMessageSource.setDefaultEncoding("UTF-8")
