bbe59237f167aaac53d251064aa08406caaf12da
Yuan Chen <godec52217@gmail.com>
2024-09-20 11:52:29 +0200

    Add Participant Data endpoint for researcher

--- src/main/kotlin/dk/cachet/carp/webservices/deployment/service/ParticipationService.kt
+++ src/main/kotlin/dk/cachet/carp/webservices/deployment/service/ParticipationService.kt
@@ -1,7 +1,10 @@
 package dk.cachet.carp.webservices.deployment.service
 
 import dk.cachet.carp.deployments.infrastructure.ParticipationServiceDecorator
+import dk.cachet.carp.deployments.infrastructure.ParticipationServiceRequest
 
 interface ParticipationService {
     val core: ParticipationServiceDecorator
+
+    suspend fun participationDataRequest(request: ParticipationServiceRequest<*>)
 }
