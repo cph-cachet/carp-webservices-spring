cdcb343af8156328718ec4599a69657dd7bc41f5
Yuan Chen <godec52217@gmail.com>
2024-09-30 17:07:27 +0200

    Fix class casting issue for participant data

--- src/main/kotlin/dk/cachet/carp/webservices/deployment/service/ParticipationService.kt
+++ src/main/kotlin/dk/cachet/carp/webservices/deployment/service/ParticipationService.kt
@@ -1,10 +1,7 @@
 package dk.cachet.carp.webservices.deployment.service
 
 import dk.cachet.carp.deployments.infrastructure.ParticipationServiceDecorator
-import dk.cachet.carp.deployments.infrastructure.ParticipationServiceRequest
 
 interface ParticipationService {
     val core: ParticipationServiceDecorator
-
-    suspend fun participationDataRequest(request: ParticipationServiceRequest<*>)
 }
