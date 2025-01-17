89a04d0d17ba0a33d2a2347d0e73aaa7c197bc0b
Nicolai Pavliuc <pavliuc75@gmail.com>
2025-01-17 16:16:23 +0100

    fix flyway migr

--- src/main/resources/db/migration/V6__restructure_file_system.sql
+++ src/main/resources/db/migration/V6__restructure_file_system.sql
@@ -4,10 +4,16 @@ DELETE FROM public.files;
 DELETE FROM public.exports;
 
 ALTER TABLE public.files
-    ADD COLUMN owner_id VARCHAR(255),
-ADD COLUMN deployment_id VARCHAR(255),
-ADD COLUMN relative_path VARCHAR(1000),
-RENAME COLUMN storage_name TO file_name;
+    ADD COLUMN owner_id VARCHAR(255);
+
+ALTER TABLE public.files
+    ADD COLUMN deployment_id VARCHAR(255);
+
+ALTER TABLE public.files
+    ADD COLUMN relative_path VARCHAR(1000);
+
+ALTER TABLE public.files
+    RENAME COLUMN storage_name TO file_name;
 
 ALTER TABLE public.exports
     ADD COLUMN relative_path VARCHAR(1000);
