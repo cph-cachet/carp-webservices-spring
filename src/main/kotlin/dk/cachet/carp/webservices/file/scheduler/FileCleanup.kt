aeb38d05184a260bd9c458d98c1d447bacbf244b
Nicolai Pavliuc <pavliuc75@gmail.com>
2025-02-12 14:05:38 +0100

    moved cleanup retention days arg to application yml

--- src/main/kotlin/dk/cachet/carp/webservices/file/scheduler/FileCleanup.kt
+++ src/main/kotlin/dk/cachet/carp/webservices/file/scheduler/FileCleanup.kt
@@ -3,21 +3,22 @@ package dk.cachet.carp.webservices.file.scheduler
 import dk.cachet.carp.webservices.file.service.FileService
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
 class FileCleanup(
     private val fileService: FileService,
+    @Value("\${cleanup.files.retention-days}") private val days: Int,
 ) {
     companion object {
         private val LOGGER: Logger = LogManager.getLogger()
     }
 
-    @Suppress("MagicNumber")
     @Scheduled(cron = "0 0 9 * * ?")
     fun cleanup() {
         LOGGER.info("Cleaning up files...")
-        fileService.deleteAllOlderThan(7)
+        fileService.deleteAllOlderThan(days)
     }
 }
