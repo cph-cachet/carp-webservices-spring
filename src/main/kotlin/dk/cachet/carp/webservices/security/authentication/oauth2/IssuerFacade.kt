de514371f112389f59fcddc9b3673be18c2acf9c
Nicolai Pavliuc <pavliuc75@gmail.com>
2025-02-14 10:25:58 +0100

    Show a list of redirect uri (#217)
    * added controller retrieveing redirectUris
    
    ---------
    
    Co-authored-by: Yuan Chen <10835085+yuanchen233@users.noreply.github.com>

--- src/main/kotlin/dk/cachet/carp/webservices/security/authentication/oauth2/IssuerFacade.kt
+++ src/main/kotlin/dk/cachet/carp/webservices/security/authentication/oauth2/IssuerFacade.kt
@@ -7,6 +7,7 @@ import dk.cachet.carp.webservices.security.authentication.oauth2.issuers.keycloa
 import dk.cachet.carp.webservices.security.authorization.Claim
 import dk.cachet.carp.webservices.security.authorization.Role
 
+@Suppress("TooManyFunctions")
 interface IssuerFacade {
     suspend fun createAccount(account: Account): Account
 
@@ -38,4 +39,6 @@ interface IssuerFacade {
         redirectUri: String?,
         expirationSeconds: Long?,
     ): String
+
+    suspend fun getRedirectUrisForClient(): List<String>
 }
